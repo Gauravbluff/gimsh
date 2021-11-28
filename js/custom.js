d7cfba3370e562d1cfc8b5758cc88bbd5fae24a1
gauravbluff <gaurav.kumarcsk@gmail.com>
2021-11-28 16:20:13 +0530

    12th update

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,11 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
+    stagePadding: 50,
     margin:10,
     mouseDrag: true,
     touchDrag: true,
     pullDrag: true,
     autoplay:true,
-    animateOut: 'fadeOut',
     nav:true,
     responsive:{
         0:{
@@ -21,36 +21,3 @@ $('.owl-carousel').owlCarousel({
 })
 
 
-
-
-var lFollowX = 0,
-    lFollowY = 0,
-    x = 0,
-    y = 0,
-    friction = 1 / 30;
-
-function moveBackground() {
-  x += (lFollowX - x) * friction;
-  y += (lFollowY - y) * friction;
-  
-  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
-
-  $('.move').css({
-    '-webit-transform': translate,
-    '-moz-transform': translate,
-    'transform': translate
-  });
-
-  window.requestAnimationFrame(moveBackground);
-}
-
-$(window).on('mousemove click', function(e) {
-
-  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-  lFollowX = (50 * lMouseX) / 50; // 100 : 12 = lMouxeX : lFollow
-  lFollowY = (5 * lMouseY) / 50;
-
-});
-
-moveBackground();
